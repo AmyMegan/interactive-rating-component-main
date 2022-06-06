4493410a71293e3e071cc98996429eef7e56da17
AmyMegan <amycostall@outlook.com>
2022-06-06 16:04:02 +0100

    Updated HTML & Javascript
    This include function of form being submitted.

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ function submit() {
     let ratingThree = document.getElementById("3");
     let ratingFour = document.getElementById("4");
     let ratingFive = document.getElementById("5");
+    const feedback = document.getElementById("feedback")
 
-    if(ratingOne.checked==true)
-    console.log(`You selected ${ratingOne.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`);
 
-    else if (ratingTwo.checked==true)
-    console.log(`You selected ${ratingTwo.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`);
-
+    if(ratingOne.checked==true) { 
+    feedback.innerHTML = `You selected ${ratingOne.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
+    }
+    else if (ratingTwo.checked==true) {
+    feedback.innerHTML = `You selected ${ratingTwo.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
+    }
     if(ratingThree.checked==true)
-    console.log(`You selected ${ratingThree.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`);
+    feedback.innerHTML = `You selected ${ratingThree.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
 
     else if (ratingFour.checked==true)
-    console.log(`You selected ${ratingFour.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`);
+    feedback.innerHTML = `You selected ${ratingFour.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
 
     if(ratingFive.checked==true)
-    console.log(`You selected ${ratingFive.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`);
+    feedback.innerHTML = `You selected ${ratingFive.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
 }
