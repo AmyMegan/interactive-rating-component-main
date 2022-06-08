ed7f23ab4cd9487dc80945c836116eb91219e552
AmyMegan <amycostall@outlook.com>
2022-06-08 07:04:56 +0100

    HTML Updated
    This will now display the data on the page in full when you submit your rating! :+1:

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function submit() {
     let ratingThree = document.getElementById("3");
     let ratingFour = document.getElementById("4");
     let ratingFive = document.getElementById("5");
-    const feedback = document.getElementById("feedback")
+    
+    const feedback = document.getElementById("container")
 
 
-    if(ratingOne.checked==true) { 
+    if(ratingOne.checked == true ) { 
     feedback.innerHTML = `You selected ${ratingOne.value} of 5 Thank you! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
     }
     else if (ratingTwo.checked==true) {
